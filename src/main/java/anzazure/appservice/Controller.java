5ec00a0d51aada58bb5fae5e584c70376cb5054d
joe tellis <awsjoetelllis.gmail.com>
2024-06-26 09:54:13 +0530

    adding to azure resource

---
+++ src/main/java/anzazure/appservice/Controller.java
@@ -0,0 +1,21 @@
+package anzazure.appservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller 
+{
+	
+    @GetMapping("/all")
+	public String getData()
+	{
+		return "My data";
+	}
+   @GetMapping("/anz/{name}")
+   public String getName(@PathVariable String name)
+   {
+	   return  "Hello" + name;
+   }
+}
---
+++ src/main/java/anzazure/appservice/User.java
@@ -0,0 +1,21 @@
+package anzazure.appservice;
+
+public class User 
+{
+private int user_Id;
+private String user_Name;
+	
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public User(int user_Id, String user_Name) {
+		super();
+		this.user_Id = user_Id;
+		this.user_Name = user_Name;
+	}
+
+}
